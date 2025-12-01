e611d5b8389a765d7281c378648c631260ad4343
Younes-ch <younes.jmell10@gmail.com>
2025-12-01 20:03:49 +0100

    feat: Integrate ErrorBoundary component for improved error handling in the application.

--- src/SkribblAI.Web/src/App.tsx
+++ src/SkribblAI.Web/src/App.tsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import { SignalRProvider } from "@/context/SignalRContext";
 import AppRouter from "@/components/Router/AppRouter";
+import ErrorBoundary from "@/components/Common/ErrorBoundary";
 
 function App() {
   return (
-    <SignalRProvider>
-      <AppRouter />
-    </SignalRProvider>
+    <ErrorBoundary>
+      <SignalRProvider>
+        <AppRouter />
+      </SignalRProvider>
+    </ErrorBoundary>
   );
 }
 
