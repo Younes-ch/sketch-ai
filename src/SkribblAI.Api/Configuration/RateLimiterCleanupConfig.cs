01fc0eed05222314e08062beb5e3178c815b4f01
Younes-ch <younes.jmell10@gmail.com>
2025-12-24 20:47:17 +0100

    feat: Add validation attributes for CleanupInterval and IdleThreshold in RateLimiterCleanupConfig.

--- src/SkribblAI.Api/Configuration/RateLimiterCleanupConfig.cs
+++ src/SkribblAI.Api/Configuration/RateLimiterCleanupConfig.cs
@@ -2,6 +2,9 @@
 
 public class RateLimiterCleanupConfig
 {
+    [Range(typeof(TimeSpan), "00:00:01", "1.00:00:00", ErrorMessage = "CleanupInterval must be between 1 second and 1 day")]
     public TimeSpan CleanupInterval { get; set; } = TimeSpan.FromMinutes(10);
+
+    [Range(typeof(TimeSpan), "00:00:01", "7.00:00:00", ErrorMessage = "IdleThreshold must be between 1 second and 7 days")]
     public TimeSpan IdleThreshold { get; set; } = TimeSpan.FromHours(1);
 }
