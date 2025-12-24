eeadb04359bc6a8c1663b7a9bed171bc5b02fcf4
Younes-ch <younes.jmell10@gmail.com>
2025-12-24 15:22:22 +0100

    refactor: Organize global using directives for better readability.

--- src/SkribblAI.Api/GlobalUsings.cs
+++ src/SkribblAI.Api/GlobalUsings.cs
@@ -1,17 +1,20 @@
-﻿global using Scalar.AspNetCore;
-global using Microsoft.AspNetCore.SignalR;
-global using SkribblAI.Api.Dtos;
-global using SkribblAI.Api.Hubs;
-global using SkribblAI.Api.Models;
-global using SkribblAI.Api.Constants;
-global using StackExchange.Redis;
-global using System.Text.Json;
+﻿global using System.Text.Json;
 global using System.Text.RegularExpressions;
-global using SkribblAI.Api.Services;
-global using SkribblAI.Api.Validation;
-global using SkribblAI.Api.Hubs.Filters;
+
+global using Microsoft.AspNetCore.SignalR;
+global using Microsoft.Extensions.Options;
+
+global using Scalar.AspNetCore;
+
+global using SkribblAI.Api.Configuration;
+global using SkribblAI.Api.Constants;
+global using SkribblAI.Api.Dtos;
 global using SkribblAI.Api.Extensions;
 global using SkribblAI.Api.Helpers;
-global using SkribblAI.Api.Configuration;
+global using SkribblAI.Api.Hubs;
+global using SkribblAI.Api.Hubs.Filters;
+global using SkribblAI.Api.Models;
+global using SkribblAI.Api.Services;
+global using SkribblAI.Api.Validation;
 
-global using Microsoft.Extensions.Options;
+global using StackExchange.Redis;
