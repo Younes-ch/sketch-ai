c9516306c6e25c3e5a756a41783751d571aa5486
Younes-ch <younes.jmell10@gmail.com>
2026-02-25 23:11:15 +0100

    fix: remove duplicate export of ReactionBanner and ReactionType in effects index.

--- src/SketchAI.Web/src/components/effects/index.ts
+++ src/SketchAI.Web/src/components/effects/index.ts
@@ -2,4 +2,3 @@ export { Confetti } from "./Confetti";
 export { useConfetti } from "@/hooks/useConfetti";
 export { ScorePopup } from "./ScorePopup";
 export { ReactionBanner } from "./ReactionPopup";
-export type { ReactionType } from "./ReactionPopup";
